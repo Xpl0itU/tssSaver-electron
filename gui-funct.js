e1e078e596143b7ced3d7ebedb9eccec4245aa64
Samuel <samuelcarballogarcia8@gmail.com>
2020-09-28 15:13:06 +0200

    Add device data grabbing (ideviceinfo)

--- gui-funct.js
+++ gui-funct.js
@@ -2,14 +2,37 @@ const electron = require('electron').remote;
 var fs = require('fs-extra');
 const path = require('path');
 const dialog = require('electron').remote.dialog;
+const process = require('child_process');
 
 function appendOutput(msg) { getCommandOutput().value = (msg+'\n'); };
 function setStatus(msg)    { getStatus().innerHTML = msg; };
 function arg() { return document.getElementsByClassName("textinputs");  };
 
+function getDeviceData() {
+
+  var ideviceinfo = './bin/ideviceinfo/ideviceinfo'
+  var getecid = process.spawn(ideviceinfo, ['-k' + 'DieID']);
+
+  getecid.stdout.on('data', (data) => {
+    document.getElementById('ecid').value = data;
+});
+
+  var getptype = process.spawn(ideviceinfo, ['-k' + 'ProductType']);
+
+  getptype.stdout.on('data', (data) => {
+    document.getElementById('device').value = data;
+  });
+
+  getptype.stderr.on('data', (error) => {
+  console.log(`${error}`);
+});
+
+
+};
+
+
 function backgroundProcess() {
-    const process = require('child_process');
-    var original = __dirname + '/tsschecker';
+    var original = __dirname + '/bin/tssChecker/tsschecker';
     var userData = electron.app.getPath('userData');
     var cmd = userData + '/' + 'tsschecker';
     fs.ensureDir(userData);
